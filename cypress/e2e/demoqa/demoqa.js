5e658fb6bd12250b42cfcb21f2a789ced16ab1b9
Jansi Rani Vethakkan <jansiranivethakkan@cgdlpt012680.home>
2024-06-08 11:37:29 +0100

    validated the text input

--- cypress/e2e/demoqa/demoqa.js
+++ cypress/e2e/demoqa/demoqa.js
@@ -1,4 +1,3 @@
-
 Given(/^I launch the demoqa Site$/, () => {
   cy.visit("/");
   cy.wait(1000);
@@ -7,7 +6,6 @@ Given(/^I launch the demoqa Site$/, () => {
 When(/^I check the title of the demoqa page$/, () => {
   cy.title();
 });
-
 
 Then("I should see the title as {string}", (title) => {
   cy.title().should("include", title);
@@ -27,19 +25,35 @@ Then(/^I should see the Tools QA page$/, () => {
 });
 
 When(/^I click on elements link$/, () => {
-  cy.get(":nth-child(1) > :nth-child(1) > .card-body > h5", { timeout: 20000 }).click();
+  cy.get(":nth-child(1) > :nth-child(1) > .card-body > h5", {
+    timeout: 20000,
+  }).click();
 });
 
 And(/^I should be able to fill the form$/, () => {
   cy.get("#userName").should("be.visible");
   cy.get("#userName").type("user");
+  cy.get("#userEmail").type("Jansi1@gmail.com");
+  cy.get("#currentAddress").type("12, redbridge lane, IG45DG");
+  cy.get("#permanentAddress").type("same as current address");
 });
 
 Then(/^I Submit the form$/, () => {
-cy.get("#submit").should("be.visible");
-cy.get("#submit").click();
+  cy.get("#submit").should("be.visible");
+  cy.get("#submit").click();
 });
 
 And(/^I should see the entered message below$/, () => {
-cy.get("#name").should("be.visible");
+  const addedUserName = `Name:user`;
+  const addedEmail = `Email:Jansi1@gmail.com`;
+  const addedCurrAdd = `Current Address :12, redbridge lane, IG45DG`;
+  const addedPerAdd = `Permananet Address :same as current address`;
+  //Get the message from a text box
+  cy.contains("#name", addedUserName);
+  cy.contains("#email", addedEmail);
+  cy.contains(".border > #currentAddress", addedCurrAdd); //should
+  cy.contains(".border > #permanentAddress", addedPerAdd); //should
+  // cy.get("#email p").should(($p) => {
+  //   const shownEmail = $p.text().trim();
+  //   expect(shownEmail).to.equal(addedEmail);
 });
