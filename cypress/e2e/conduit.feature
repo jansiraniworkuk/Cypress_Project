cb84ad2a895d88599c80e96966bf20ff0f66ef2f
Jansi Rani <jansiraniworkuk@gmail.com>
2024-06-21 14:41:33 +0100

    Added test cases for conduit elements verification on signin page

--- cypress/e2e/conduit.feature
+++ cypress/e2e/conduit.feature
@@ -1,9 +1,18 @@
 Feature: To validate the conduit application
 
-    @conduit
-    Scenario: Launch the Conduit application and quickly check the connectivity
+    @conduit @elements 
+    Scenario: Launch the Conduit application and check the elements on the sign in page
         Given I visit the conduit web page
         When I navigate to sign in page
         Then I will be able to see the signin page
+        And I will be able to see all the elements of the signin page
 
+    
+    @cnonduit @elements
+    Scenario:  Launch the Conduit application and check the elements on the home page
+        Given I visit the conduit web page
+        When I am on the home page
+        Then I will be able to see all the elements of the home page
+
+    
     
--- cypress/e2e/conduit/conduit.js
+++ cypress/e2e/conduit/conduit.js
@@ -1,3 +1,5 @@
+import testIdMap from '../../fixtures/elements.json'
+
 Given(/^I visit the conduit web page$/, () => {
   cy.visit("/");
 });
@@ -7,12 +9,47 @@ When(/^I navigate to sign in page$/, () => {
 });
 
 Then(/^I will be able to see the signin page$/, () => {
-    cy.get('h1.text-xs-center').should('have.text', 'Sign In');
+    cy.get(testIdMap['sign in page heading'])
+    .should('have.text', 'Sign In')
+    .should('have.css', 'color', 'rgb(55, 58, 60)');
 });
 
 And(/^I will be able to see all the elements of the signin page$/, () => {
-    cy.get('h1.text-xs-center').should('have.text', 'Sign In');
-    cy.get('.navbar-brand').should('have.text', 'conduit');
-    cy.get('p.text-xs-center > a').should('have.text', 'Need an account');  
-});
+    //verify brand icon
+    cy.get(testIdMap['brand icon'])
+    .should('have.text', 'conduit')
+    .should('have.css', 'color', 'rgb(92, 184, 92)');
 
+    //verify registration link
+    cy.get(testIdMap['register link from sign in page'])
+    .should('have.text', 'Need an account?')
+    .should('have.attr', 'href', '#register')
+    .should('have.css', 'color', 'rgb(92, 184, 92)');  
+    
+    //verify the navigation links
+    cy.get(testIdMap['navigation link home'])
+    .should('have.text', 'Home')
+    .should('have.attr', 'href', '#/')
+    .should('have.css', 'color', 'rgba(0, 0, 0, 0.3)');
+
+    cy.get(testIdMap['navigation link sign-in'])
+    .should('have.text', 'Sign in')
+    .should('have.attr', 'href', '#login');
+
+    cy.get(testIdMap['navigation link sign up'])
+    .should('have.text', 'Sign up')
+    .should('have.attr', 'href', '#register');
+   
+    //verify input boxes
+    cy.get(testIdMap['email input box from sign in page'])
+    .should('have.attr', 'placeholder', 'Email');
+
+    cy.get(testIdMap['password input box from sign in page'])
+    .should('have.attr', 'placeholder', 'Password');
+
+    //validate sign in button
+    cy.get(testIdMap['sign in button'])
+    .should('have.text', 'Sign in')
+    .should('have.css', 'color','rgb(255, 255, 255)')
+    .should('have.css', 'background-color','rgb(92, 184, 92)');
+});
